135cfa1700bd432a89d90ea11cb42442d5187052
manish <manishtapase2@gmail.com>
2025-04-02 00:08:01 +0530

    file updated

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React, { Suspense } from "react";
-import { Route, Routes } from "react-router-dom";
-
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./styles/font.css";
+
 const LandingPage = React.lazy(() => import("./Components/LandingPage"));
 const Main = React.lazy(() => import("./Sections/Main"));
+
 class ErrorBoundary extends React.Component {
   state = { hasError: false };
 
@@ -12,7 +13,7 @@ class ErrorBoundary extends React.Component {
   }
 
   componentDidCatch(error, errInfo) {
-    console.error("ErrorBoundary caugth :", error, errInfo);
+    console.error("ErrorBoundary caught:", error, errInfo);
   }
 
   render() {
@@ -24,24 +25,67 @@ class ErrorBoundary extends React.Component {
   }
 }
 
-const routes = [
-  // { path: "/", component: LandingPage },
-  { path: "/home", component: Main },
-];
-
 const App = () => {
   return (
-    <ErrorBoundary>
-      <Suspense fallback={<div></div>}>
-        <Routes>
-          <Route path="*" element={<LandingPage />} />
-          {routes.map(({ path, component: Component }, index) => (
-            <Route key={index} path={`${path}`} element={<Component />} />
-          ))}
-        </Routes>
-      </Suspense>
-    </ErrorBoundary>
+    <Router basename="/wattwave">
+      <ErrorBoundary>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Routes>
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/home" element={<Main />} />
+          </Routes>
+        </Suspense>
+      </ErrorBoundary>
+    </Router>
   );
 };
 
 export default App;
+
+// import React, { Suspense } from "react";
+// import { Route, Routes } from "react-router-dom";
+
+// import "./styles/font.css";
+// const LandingPage = React.lazy(() => import("./Components/LandingPage"));
+// const Main = React.lazy(() => import("./Sections/Main"));
+// class ErrorBoundary extends React.Component {
+//   state = { hasError: false };
+
+//   static getDerivedStateFromError() {
+//     return { hasError: true };
+//   }
+
+//   componentDidCatch(error, errInfo) {
+//     console.error("ErrorBoundary caugth :", error, errInfo);
+//   }
+
+//   render() {
+//     if (this.state.hasError) {
+//       return <div>Something went wrong. Please try again later.</div>;
+//     } else {
+//       return this.props.children;
+//     }
+//   }
+// }
+
+// const routes = [
+//   // { path: "/", component: LandingPage },
+//   { path: "/home", component: Main },
+// ];
+
+// const App = () => {
+//   return (
+//     <ErrorBoundary>
+//       <Suspense fallback={<div></div>}>
+//         <Routes>
+//           <Route path="*" element={<LandingPage />} />
+//           {routes.map(({ path, component: Component }, index) => (
+//             <Route key={index} path={`${path}`} element={<Component />} />
+//           ))}
+//         </Routes>
+//       </Suspense>
+//     </ErrorBoundary>
+//   );
+// };
+
+// export default App;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter } from "react-router-dom";
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-);
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import App from "./App";
+// import { HashRouter } from "react-router-dom";
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(
+//   <HashRouter>
+//     <App />
+//   </HashRouter>
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
